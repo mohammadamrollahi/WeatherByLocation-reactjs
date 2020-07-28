ef51dba6ed79b40792cfca69fde69ab7525c58f3
Mohammad Amrollahi <mohammadamrollahi3@gmail.com>
2020-07-28 11:27:16 +0430

    Updates

---
+++ precache-manifest.a4cab2263ebf20f2e3fcf4a82468db47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ee62f03163b80a1fdd25851987ad13",
+    "url": "/WeatherByLocation-reactjs/index.html"
+  },
+  {
+    "revision": "c4bf528607442a283a11",
+    "url": "/WeatherByLocation-reactjs/static/css/main.d61a5900.chunk.css"
+  },
+  {
+    "revision": "0c557c71e909bee9f1de",
+    "url": "/WeatherByLocation-reactjs/static/js/2.e16fc6fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherByLocation-reactjs/static/js/2.e16fc6fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4bf528607442a283a11",
+    "url": "/WeatherByLocation-reactjs/static/js/main.2ee40503.chunk.js"
+  },
+  {
+    "revision": "71996f938da6e6c8632a",
+    "url": "/WeatherByLocation-reactjs/static/js/runtime-main.4c0c299c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/WeatherByLocation-reactjs/static/media/logo.5d5d9eef.svg"
+  }
+]);
